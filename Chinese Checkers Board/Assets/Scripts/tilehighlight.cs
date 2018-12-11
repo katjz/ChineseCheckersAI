4401a036bed877b64438cb2a2f5e33a6e8606d6e
Peng Gu <pgu@oberlin.edu>
2018-12-10 20:37:18 -0500

    Mouse hovering solved, start working on clicking

---
+++ Chinese Checkers Board/Assets/Scripts/tilehighlight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class tilehighlight : MonoBehaviour
+{
+
+    /*public GameObject highlight;
+
+    void Update()
+    {
+        StartCoroutine(Off());
+        StartCoroutine(On());
+    }
+
+    IEnumerator Off()
+    {
+        highlight.SetActive(false);
+        yield return new WaitForSeconds(2.0f);
+    }
+
+    IEnumerator On()
+    {
+        highlight.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+    }*/
+
+}
