185a43f04ba5b531e088777c6330f90788d40b88
katjz <kzoner@oberlin.edu>
2018-12-12 11:23:18 -0500

    buttons now move between menu scenes

---
+++ RealCheckerBoard/Assets/Scripts/QuitOnClick.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOnClick : MonoBehaviour {
+
+	public void Quit()
+	{
+#if UNITY_EDITOR
+	Debug.Log("User quitting game");
+	UnityEditor.EditorApplication.isPlaying = false;
+#else
+	Application.Quit();
+#endif
+	}
+		
+}
