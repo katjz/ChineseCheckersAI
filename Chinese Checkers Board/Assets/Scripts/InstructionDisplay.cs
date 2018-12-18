77ee0350f360184633373615dcaaa500465a657a
katjz <kzoner@oberlin.edu>
2018-12-18 00:20:27 -0500

    added help button

---
+++ Chinese Checkers Board/Assets/Scripts/InstructionDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstructionDisplay : MonoBehaviour
+{
+	GameObject canvas;
+	GameObject helpTitle;
+	GameObject helpText;
+	public bool help = false;
+
+	public void Start(){
+		canvas = GameObject.Find ("Canvas");
+		helpTitle = canvas.transform.Find ("HelpTitle").gameObject;
+		helpText = canvas.transform.Find ("HelpText").gameObject;
+	}
+	public void display(){
+		if (!help) {
+			help = true;
+			helpText.SetActive (true);
+			helpTitle.SetActive (true);
+			//StartCoroutine (waitToHide ());
+		} else {
+			help = false;
+			helpText.SetActive (false);
+			helpTitle.SetActive (false);
+		}
+	}
+
+	IEnumerator waitToHide(){
+		yield return new WaitForSeconds (10.0f);
+		helpText.SetActive (false);
+		helpTitle.SetActive (false);
+	}
+}
